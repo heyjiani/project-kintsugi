942185b2669c8212e44f99d993b3a2eee533b7b8
Lindsay <lbtannahill@gmail.com>
2022-03-19 20:56:44 -0700

    created post route for new appointments

--- express-back-end/routes/appointments.js
+++ express-back-end/routes/appointments.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+import { newAppointment } from "./db/helper/appointment-queries.js"
 
 //all the appointment routes come here//
 
@@ -8,7 +9,14 @@ const appointmentRoutes = () => {
       data: "Hello from the appointments API!",
     });
   });
-
+  // router.post("/", (req, res) => {
+  //   // console.log(req.body)
+  //   const client_id = req.body.client_id
+  //   const prof_id = req.body.prof_id
+  //   const appointment_data = req.body.appointment_data
+  //   newAppointment(client_id, prof_id, appointment_data)
+  // });
+  
   return router;
 };
 
