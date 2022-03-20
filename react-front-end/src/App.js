984ee9196b8b38fa2fd770005831345e65797db3
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-03-20 16:53:20 -0700

    add routes and router in app.js

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,67 +1,82 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './css/App.css';
+/* eslint-disable no-undef */
+import React from "react";
+// import axios from "axios";
+import "./css/App.css";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 
-class App extends Component {
- 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
+import Homepage from "./pages/Homepage";
+import ProfessionalsPage from "./pages/ProfessionalsPage";
+import AppointmentPage from "./pages/AppointmentPage";
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    }) 
-  }
+// fetchData = () => {
+//   axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+//   .then((response) => {
+//     // handle success
+//     console.log(response.data) // The entire response from the Rails API
 
-  render() {
-    return (
-<body>
+//     console.log(response.data.message) // Just the message
+//     this.setState({
+//       message: response.data.message
+//     });
+//   })
+// }
 
+function App() {
+  return (
+    <body>
+      {/* nav bar below  */}
 
-  {/* nav bar below  */}
+      <nav class="navigation">
+        <div class="logo">
+          <h1>Kintsugi</h1>
+        </div>
+      </nav>
 
-    <nav>
+      {/* the components will populate in the "components_container" div below */}
 
-      <div className='logo'>
-      <h1>Kintsugi</h1>
-      </div>
+      <main>
+        <div className="components_container">
+          the componnents will go here
+        </div>
+      </main>
 
-      <div className='navigation_links'>
-        <div>About</div><div>Get</div><div>View Appointments</div>
-      </div>
+      {/* footer below  */}
 
-    </nav> 
+      <footer>
+        <div>
+          <h4>Kintsugi</h4>
+        </div>
+        <div>About the makers.</div>
+        <div>© Kintsugi 2022</div>
+      </footer>
 
-
-{/* the components will populate in the "components_container" div below */}
-
-  <main>
-      <div className="components_container">
-        the componnents will go here 
-      </div>
-  </main>
-
-{/* footer below  */}
-
-<footer>
-  <div>
-<h4>Kintsugi</h4>
-</div>
-<div>
-About the makers. 
-</div>
-<div>
-© Kintsugi 2022
-</div>
-</footer>
-
-      </body>
-    );
-  }
+      <BrowserRouter>
+        {/* <Nav/ > */}
+        <Routes>
+          <Route
+            path="/"
+            element={<Homepage />}
+            exact
+          />
+          <Route
+            path="/professionals"
+            element={<ProfessionalsPage />}
+            exact
+          />
+          <Route
+            path="/form"
+            element={<AppointmentPage />}
+            exact
+          />
+          {/* <Route element={<Error />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </body>
+  );
 }
 
 export default App;
