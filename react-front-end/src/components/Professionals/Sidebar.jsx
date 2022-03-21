98a7e81794d43464ff391c65f0027761b818fdfe
Jiani Cao <jianicakes@gmail.com>
2022-03-21 13:02:40 -0700

    add & import stylings

--- react-front-end/src/components/Professionals/Sidebar.jsx
+++ react-front-end/src/components/Professionals/Sidebar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+import "../../scss/Sidebar.scss"
+
 export default function Sidebar() {
   return (
     <aside className="professionals__sidebar">
       <header>Sort</header>
       <div>
-        City
+        <h4>City</h4>
         <input type="checkbox" />Item 1
         <input type="checkbox" />Item 2
         <input type="checkbox" />Item 3
