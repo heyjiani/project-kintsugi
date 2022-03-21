b2c702219569590d17d931abbeb9e9997ff1f6fe
Jiani Cao <jianicakes@gmail.com>
2022-03-20 23:12:01 -0700

    set up parent list component

---
+++ react-front-end/src/components/AppointmentList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import AppointmentListItem from "./AppointmentListItem";
+
+export default function AppointmentList() {
+  return (
+    <section>
+      <ul><AppointmentListItem /></ul>
+    </section>
+  )
+}
