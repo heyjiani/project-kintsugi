c97157ea9b790ce4d2495f536e4d1c5b602e743d
Jiani Cao <jianicakes@gmail.com>
2022-03-20 23:21:40 -0700

    add icon and labels

--- react-front-end/src/components/AppointmentListItem.jsx
+++ react-front-end/src/components/AppointmentListItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 export default function AppointmentListItem() {
+  // component has a dropdown on click //
 
   return (
     <li>
       <div>2022-06-07</div>
       <div>some time</div>
       <div>Insert info about appointment</div>
+      <img src="https://cdn-icons-png.flaticon.com/128/1946/1946429.png" alt=""/>
     </li>
   )
 }
