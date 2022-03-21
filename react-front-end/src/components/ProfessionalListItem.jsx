850b409139135d4ab4c5d10d9c31dfd35c0f3875
Jiani Cao <jianicakes@gmail.com>
2022-03-20 19:09:08 -0700

    set up component

---
+++ react-front-end/src/components/ProfessionalListItem.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+export default function ProfessionalListItem() {
+  return (
+    <section>
+      <header>
+        <h2>Yumi Tanaka</h2>
+      </header>
+      <div>
+        <img src="https://cdn-icons-png.flaticon.com/128/1946/1946429.png"/>
+        <ul>
+          <li>Therapist</li>
+          <li>Family Conflict</li>
+          <li>Campbell River, BC</li>
+        </ul>
+      </div>
+      <footer>
+        <button>Find out more</button>
+      </footer>
+    </section>
+  )
+}
