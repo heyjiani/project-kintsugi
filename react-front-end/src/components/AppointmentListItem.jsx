156bae3e915b272a09a0cca43dd1fda1a72a39f7
Jiani Cao <jianicakes@gmail.com>
2022-03-20 23:10:32 -0700

    set up appointment item component

---
+++ react-front-end/src/components/AppointmentListItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AppointmentListItem() {
+
+  return (
+    <li>
+      <div>2022-06-07</div>
+      <div>some time</div>
+      <div>Insert info about appointment</div>
+    </li>
+  )
+}
