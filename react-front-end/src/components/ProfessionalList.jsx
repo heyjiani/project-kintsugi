50cc9e5cf98fac0581d8b8e1644c8ddeb8b39e0b
Jiani Cao <jianicakes@gmail.com>
2022-03-20 23:04:56 -0700

    add flex styling to list

--- react-front-end/src/components/ProfessionalList.jsx
+++ react-front-end/src/components/ProfessionalList.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import ProfessionalListItem from "./ProfessionalListItem";
 
+import "../scss/ProfessionalList.scss";
+
 export default function ProfessionalList() {
   
   // rendering three repeats for now //
   return (
     <section className="professionals">
-      <ul>
+      <ul className="professionals__list">
         <ProfessionalListItem />
         <ProfessionalListItem />
         <ProfessionalListItem />
