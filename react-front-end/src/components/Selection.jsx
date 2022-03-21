5e8da65410522a2727b74de247dd0cf106ff235f
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-03-21 11:28:09 -0700

    create seliectionlist file

--- react-front-end/src/components/Selection.jsx
+++ react-front-end/src/components/Selection.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 export default function Selection(props) {
+  //click and dropdown the list of genre//
+
   return (
     <div className="selection">{props.genre}</div>
   );
