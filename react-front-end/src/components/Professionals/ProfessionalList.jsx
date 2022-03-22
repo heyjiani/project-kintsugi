79cdc7ae62702d5a8476a61972c41f9b234579db
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-03-22 13:20:29 -0700

    make jump to the booking/:id fetch works

--- react-front-end/src/components/Professionals/ProfessionalList.jsx
+++ react-front-end/src/components/Professionals/ProfessionalList.jsx
@@ -1,24 +1,31 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from 'axios';
+import React, {
+  useState,
+  useEffect,
+} from "react";
+
+import axios from "axios";
 
 import ProfessionalListItem from "./ProfessionalListItem";
 
 export default function ProfessionalList(props) {
   const { professionals } = props;
-  const navigate = useNavigate();
-  const handleOnClick = () => navigate('/booking');
 
   const parsedProfessionalData =
-    Array.isArray(professionals) && professionals.map(p => {
-      return <ProfessionalListItem key={p.id} {...p} onClick={handleOnClick}/>
+    Array.isArray(professionals) &&
+    professionals.map((p) => {
+      return (
+        <ProfessionalListItem key={p.id} {...p} />
+      );
     });
 
   return (
     <div className="professionals__container">
-      <h2 className="display-info">Displaying professionals who speak Japanese in BC</h2>
+      <h2 className="display-info">
+        Displaying professionals who speak
+        Japanese in BC
+      </h2>
       <ul className="professionals__list">
-      { parsedProfessionalData }
+        {parsedProfessionalData}
       </ul>
     </div>
   );
--- react-front-end/src/components/Professionals/ProfessionalListItem.jsx
+++ react-front-end/src/components/Professionals/ProfessionalListItem.jsx
@@ -1,13 +1,33 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function ProfessionalListItem(props) {
-  const {first_name, last_name, city, province, profession, icon_url, language_1} = props;
+export default function ProfessionalListItem(
+  props
+) {
+  const {
+    first_name,
+    last_name,
+    city,
+    province,
+    profession,
+    icon_url,
+    language_1,
+    id,
+  } = props;
   const professionalClass = "professionals__item";
+
+  const navigate = useNavigate();
+  const handleOnClick = () => {
+    console.log(id);
+    navigate(`/booking/${id}`);
+  };
 
   return (
     <li className={professionalClass}>
       <header>
-        <h2>{first_name} {last_name}</h2>
+        <h2>
+          {first_name} {last_name}
+        </h2>
       </header>
       <div>
         <img
@@ -18,11 +38,15 @@ export default function ProfessionalListItem(props) {
         <ul>
           <li>{profession}</li>
           <li>{language_1}</li>
-          <li>{city}, {province}</li>
+          <li>
+            {city}, {province}
+          </li>
         </ul>
       </div>
       <footer>
-        <button onClick={() => props.onClick()}>Find out more</button>
+        <button onClick={() => handleOnClick(id)}>
+          Find out more
+        </button>
       </footer>
     </li>
   );
