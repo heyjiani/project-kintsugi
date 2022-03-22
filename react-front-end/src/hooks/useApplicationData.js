97c3822d3ee9a29c8a6fcd4885d6ee5266b4d6ae
Jiani Cao <jianicakes@gmail.com>
2022-03-22 16:07:41 -0700

    setup master file for data fetch

--- react-front-end/src/hooks/useApplicationData.js
+++ react-front-end/src/hooks/useApplicationData.js
@@ -1,7 +1,37 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-useEffect(() => {
-  Promise.all([
-    // axios.get()
-  ]).then()
-}, []);
+export default function useApplicationData() {
+
+  const [state, setState] = useState({
+    professionals: [],
+    specialties: [],
+    appointments: []
+  });
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/professionals"),
+      axios.get("/api/appointments"),
+      axios.get("/api/specialties")
+    ]).then(all => {
+      setState(prev => ({
+        ...prev,
+        professionals: all[0].data,
+        appointments: all[1].data,
+        specialties: all[2].data
+      }))
+    })
+  }, []);
+  
+
+  // const findProfessionalById = (id) => {
+  //   return state.professionals.filter(p => p.id === id);
+  // };
+
+  // return {
+  //   state,
+  //   findProfessionalById
+  // };
+
+};
