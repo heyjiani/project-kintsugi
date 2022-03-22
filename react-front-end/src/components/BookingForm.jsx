202043af1b2e17ba3aa4868403ec1fc9ca62f6a5
Lindsay <lbtannahill@gmail.com>
2022-03-21 17:10:52 -0700

    added calendar and time list to booking page and created seperate component file

---
+++ react-front-end/src/components/BookingForm.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import {useState} from "react";
+import Calendar, { CenturyView } from 'react-calendar';
+
+export default function BookingForm() {
+  const [date, setDate] = useState(new Date());
+  return (
+    <div
+        className="form_container">
+        <div>
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/3062/3062992.png"
+            style={{ width: 100}}
+            alt=""
+          ></img>
+        </div>
+        <div>
+          Yumi Tanaka
+          <br />
+          <table>
+            <tr>image
+            <td>therapist
+            </td>
+            </tr>
+            <tr>image
+            <td>ADHD, Depression
+            </td>
+            </tr>
+            <tr>image
+            <td>Squamish, BC
+            </td>
+            </tr>
+          </table>
+        </div>
+        <div>
+        <form class="center" action="/" method="POST" id="submitRequest">
+          Select appointment date: <Calendar onChange={setDate} value={date} />
+          Select appointment time: 
+          <ul>
+          <li>9:00am</li>
+          <li>10:00am</li>
+          <li>01:00pm</li>
+          <li>02:00pm</li>
+          <li>04:00pm</li>
+          </ul>
+          <textarea style={{width: 600, height: 400}} name="text" id="appointment_request" class="request" type="text" placeholder="Please provide the mental health worker with some additional details about your appointment request. If you’d prefer, select the microphone to record your message. It will translate your request from your primary language to English. "></textarea>
+            <br />
+        <button>Submit</button>
+        </form>
+        </div>
+      </div>
+  );
+}
