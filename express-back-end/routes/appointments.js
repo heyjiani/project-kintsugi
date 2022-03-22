d191356614fa9122f5fce833da8f0248830a5934
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-03-22 15:39:58 -0700

    add some queires

--- express-back-end/routes/appointments.js
+++ express-back-end/routes/appointments.js
@@ -1,17 +1,26 @@
 const router = require("express").Router();
-const { getAppointments, getSingleAppointment } = require('../db/helper/appointment-queries');
-// import { newAppointment } from "./db/helper/appointment-queries.js"
+const {
+  getAppointments,
+  getSingleAppointment,
+  getAppointmentsByUserId,
+  newAppointment,
+} = require("../db/helper/appointment-queries");
 
 //all the appointment routes come here//
 
 const appointmentRoutes = () => {
   router.get("/", (req, res) => {
-    console.log("JSON Statham")
-    getAppointments() 
-      .then(response => res.json(response))
-      .catch(e => console.log(e))
-    });
+    console.log("JSON Statham");
+    getAppointments()
+      .then((response) => res.json(response))
+      .catch((e) => console.log(e));
+  });
 
+  router.get("/all/:id", (req, res) => {
+    getAppointmentsByUserId(req.params.id)
+      .then((response) => res.json(response))
+      .catch((e) => console.log(e));
+  });
 
   // router.post("/", (req, res) => {
   //   // console.log(req.body)
@@ -20,6 +29,12 @@ const appointmentRoutes = () => {
   //   const appointment_data = req.body.appointment_data
   //   newAppointment(client_id, prof_id, appointment_data)
   // });
+
+  router.post("/", (req, res) => {
+    newAppointment(req.body)
+      .then((response) => res.json(response))
+      .catch((e) => console.log(e));
+  });
 
   return router;
 };
