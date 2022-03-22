6b9f4a14637b172aa81fd732c94c7a8bbc58c304
Jiani Cao <jianicakes@gmail.com>
2022-03-22 01:47:02 -0700

    add styling for list display

--- react-front-end/src/components/Professionals/ProfessionalList.jsx
+++ react-front-end/src/components/Professionals/ProfessionalList.jsx
@@ -6,11 +6,14 @@ export default function ProfessionalList() {
   // rendering 4 repeats for now //
 
   return (
-    <ul className="professionals__container">
+    <div className="professionals__container">
+      <h2 className="display-info">Displaying professionals who speak Japanese in BC</h2>
+      <ul className="professionals__list">
       <ProfessionalListItem />
       <ProfessionalListItem />
       <ProfessionalListItem />
       <ProfessionalListItem />
-    </ul>
+      </ul>
+    </div>
   );
 }
--- react-front-end/src/pages/ProfessionalsPage.jsx
+++ react-front-end/src/pages/ProfessionalsPage.jsx
@@ -6,7 +6,6 @@ import Sidebar from "../components/Professionals/Sidebar";
 export default function ProfessionalsPage() {
   return (
     <div className="professionals">
-      <h2>Displaying professionals who speak Japanese in BC</h2>
       <Sidebar />
       <ProfessionalList />
     </div>
