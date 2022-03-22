86d3aa6cb2be2afba02b14b1e10569f29649c00c
Jiani Cao <jianicakes@gmail.com>
2022-03-21 20:20:05 -0700

    shrink navbar

--- react-front-end/src/components/NavLinks.jsx
+++ react-front-end/src/components/NavLinks.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function NavLinks() {
   return (
-    <div className='navigation_links'>
-    <div><a href="">About</a></div>
-    <div><a href="">Get Help</a></div>
-    <div><a href="">View Appointments</a></div>
-  </div>
+    <div className='navigation__links'>
+      <div><a href="">About</a></div>
+      <div><a href="">Get Help</a></div>
+      <div><a href="">View Appointments</a></div>
+    </div>
   );
 }
