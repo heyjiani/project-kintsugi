9c072dce29b119322579b125ef5443354d50d85e
Lindsay <lbtannahill@gmail.com>
2022-03-25 19:00:59 -0700

    added tag image to sidebar

--- react-front-end/src/components/Professionals/Sidebar.jsx
+++ react-front-end/src/components/Professionals/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { AiOutlineTag } from 'react-icons/ai'
 
 export default function Sidebar(props) {
   // need to populate checkboxes with prop data //
@@ -17,12 +18,9 @@ export default function Sidebar(props) {
 
   return (
     <div className="sidebar">
-      <header>
-        <img
-          src="https://cdn-icons.flaticon.com/png/512/1620/premium/1620735.png?token=exp=1648052951~hmac=bb1eab46446957048f5bc2f90ff6e8e6"
-          alt=""
-        />{" "}
-        &nbsp;&nbsp;&nbsp;&nbsp; Sort
+      <header><span>
+      <AiOutlineTag /></span>{" "}
+         Sort
       </header>
       <div className="sidebar__options">
         <div className="sidebar__options__category">
