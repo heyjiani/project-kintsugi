020c5dd71bfc9a2cf23e1192b51ca44ff773dd9b
Jiani Cao <jianicakes@gmail.com>
2022-03-28 16:10:16 -0700

    add test for navigating to appointments page

--- react-front-end/cypress/integration/navigation.spec.js
+++ react-front-end/cypress/integration/navigation.spec.js
@@ -1,11 +1,14 @@
 describe("Navigation", () => {
 
+  beforeEach(() => {
+    cy.visit("/");
+  })
+
   xit("should visit root", () => {
     cy.visit("/");
   });
 
   it("should navigate to professionals after selecting search params", () => {
-    cy.visit("/");
 
     cy.get(".selection").contains("Language").click();
     cy.contains("Japanese").click();
@@ -18,5 +21,14 @@ describe("Navigation", () => {
     cy.contains(".professionals__item", "Garek Slamaker");
     cy.contains("professionals__item", "Ole Beagley").should("not.exist");
     cy.get(".professionals").find(".professionals__item").should("have.length", 3);
+
   });
+
+  it("should navigate to the appointment page", () => {
+    
+    cy.get(".navigation").contains("View Appointments").click();
+
+    cy.contains("Upcoming Appointments");
+    cy.get(".appointment").should("have.length", 2);
+  })
 });
