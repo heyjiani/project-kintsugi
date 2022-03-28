3b1bc385de52d4f217b034394b8a4871206325dc
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-03-28 10:36:06 -0700

    merge cypress

---
+++ react-front-end/cypress/integration/navigation.spec.js
@@ -0,0 +1,21 @@
+describe("Navigation", () => {
+
+  xit("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("should navigate to professionals after selecting search params", () => {
+    cy.visit("/");
+
+    cy.get(".selection").contains("Language").click();
+    cy.contains("Japanese").click();
+
+    cy.get(".selection").contains("Province").click();
+    cy.contains("BC").click();
+
+    cy.get(".searchbtn").click();
+
+    cy.contains(".professionals__item", "Garek Slamaker");
+    cy.contains("professionals__item", "Ole Beagley").should("not.exist");
+  });
+});
