b289c5ca9fe06abc09c554218e6945f219468816
Jiani Cao <jianicakes@gmail.com>
2022-03-28 00:32:08 -0700

    add test for search navigation

--- react-front-end/cypress/integration/navigation.spec.js
+++ react-front-end/cypress/integration/navigation.spec.js
@@ -1,5 +1,21 @@
 describe("Navigation", () => {
-  it("should visit root", () => {
+
+  xit("should visit root", () => {
     cy.visit("/");
+  });
+
+  it("should navigate to professionals after selecting search params", () => {
+    cy.visit("/");
+
+    cy.get(".selection").contains("Language").click();
+    cy.contains("Japanese").click();
+
+    cy.get(".selection").contains("Province").click();
+    cy.contains("BC").click();
+
+    cy.get(".searchbtn").click();
+
+    cy.contains(".professionals__item", "Garek Slamaker");
+    cy.contains("professionals__item", "Ole Beagley").should("not.exist");
   });
 });
