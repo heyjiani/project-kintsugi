b115cbd5c69eb2193abf1fec7a85cd9147c4cdad
Jiani Cao <jianicakes@gmail.com>
2022-03-30 00:23:51 -0700

    update pg connection for deployment

--- express-back-end/lib/db.js
+++ express-back-end/lib/db.js
@@ -8,11 +8,8 @@ const PATH = path.resolve(__dirname, "../.env." + ENV);
 require("dotenv").config({ path: PATH });
 
 const dbParams = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL || "",
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 };
 
 const db = new Pool(dbParams);
