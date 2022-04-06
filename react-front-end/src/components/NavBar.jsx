00a774351080432c58b77c287112310e625c36ba
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-04-06 13:04:31 -0700

    clean css

--- react-front-end/src/components/NavBar.jsx
+++ react-front-end/src/components/NavBar.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import NavLinks from '../components/NavLinks';
+
 import { FaRegUser } from 'react-icons/fa'
 
 export default function NavBar() {
   return (
     <nav className="navigation">
-      <div className="logo">
-        <a href="/">
-          <h1>Kintsugi</h1>
-        </a>
+      <a href="/" className="logo">
+        Kintsugi
+      </a>
+      <div className="navigation__center">
+        <div>
+          <a href="/">Get Help</a>
+        </div>
+        <div>
+          <a href="/myAppointments">View Appointments</a>
+        </div>
       </div>
-      <NavLinks />
-      <div className="navigation__links2">
-        <div className="logout"><span className="user"><FaRegUser /> Aya Okizaki</span><br /><span className="hover">Logout</span> </div>
+      <div className="navigation__right">
+        <span className="user"><FaRegUser />Aya Okizaki</span>
+        <br />
+        <span className="logout">Logout</span>
       </div>
     </nav>
   );
